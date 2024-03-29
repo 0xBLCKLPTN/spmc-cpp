25561db4d3513cb0071744f07da10f3587f421ac
Daniil Ermolaev <blcklptn@icloud.com>
2024-03-30 00:11:33 +0500

    prepare to easy triangle component

--- SourceFiles/Core/Window.cpp
+++ SourceFiles/Core/Window.cpp
@@ -5,6 +5,7 @@
 #include "./ButtonsCallback.hpp"
 #include "./PngLoader.hpp"
 #include <cstring>
+#include "../Components/Triangle.hpp"
 
 using namespace std;
 using namespace glm;
@@ -19,6 +20,10 @@ class Window
         GLFWwindow *window;
         GLFWimage cursor_image;
         double xpos, ypos;
+        Triangle tr;
+        GLuint VertexArrayID;
+        GLuint programID;
+        GLuint vertexbuffer;
 
         void initGLSettings()
         {
@@ -31,6 +36,22 @@ class Window
 	        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make macOS happy; should not be needed
 	        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // Мы не хотим старый OpenGL
             glewExperimental=true; // Флаг необходим в Core-режиме OpenGL
+
+
+            programID = LoadShaders("./data/Assets/Shaders/Triangle.vertexshader", "./data/Assets/Shaders/Triangle.fragmentshader");
+            glGenVertexArrays(1, &VertexArrayID);
+            glBindVertexArray(VertexArrayID);
+
+            static const GLfloat g_vertex_buffer_data[] = { 
+                -1.0f, -1.0f, 0.0f,
+                1.0f, -1.0f, 0.0f,
+                0.0f,  1.0f, 0.0f,
+            };
+
+            
+            glGenBuffers(1, &vertexbuffer);
+            glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+            glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
         }
 
         void initCursor()
@@ -46,7 +67,21 @@ class Window
             {
                 /* Render here */
                 glClear(GL_COLOR_BUFFER_BIT);
-            
+                
+                glUseProgram(programID);
+                glEnableVertexAttribArray(0);
+                glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+                glVertexAttribPointer(
+                    0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+                    3,                  // size
+                    GL_FLOAT,           // type
+                    GL_FALSE,           // normalized?
+                    0,                  // stride
+                    (void*)0            // array buffer offset
+                );
+                glDrawArrays(GL_TRIANGLES, 0, 3); // 3 indices starting at 0 -> 1 triangle
+                glDisableVertexAttribArray(0);
+
                 /* Swap front and back buffers */
                 glfwSwapBuffers(window);
                 
@@ -72,6 +107,9 @@ class Window
             glClearColor( 0.4f, 0.3f, 0.4f, 0.0f );
             this->render();
             deallocate_pixels(cursor_image.pixels);
+            glDeleteBuffers(1, &vertexbuffer);
+            glDeleteVertexArrays(1, &VertexArrayID);
+            glDeleteProgram(programID);
             glfwTerminate();
         }
 };
